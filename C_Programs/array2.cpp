8da4bf876899b5974dcdf5174e915698a5cc517b
GuhankannanECE <143473399+GuhankannanECE@users.noreply.github.com>
2023-10-16 17:01:52 +0530

    Add files via upload

---
+++ C_Programs/array2.cpp
@@ -0,0 +1,34 @@
+#include<stdio.h>#include<stdio.h>
+int main()
+{
+	int i,j,n;
+	printf("plaese enter the value of n:");
+	scanf("%d",&n);
+	int a[n];
+	printf("enter the numbers in the list");
+	for(i=0;i<n;i++)
+	{
+		scanf("%d",&a[i]);
+	}	
+	for(i=0;i<n;i++)
+	{
+		for(j=0;j<n;j++)
+		{
+			if(a[j]>a[i])
+			{
+				a[i]=a[i]+a[j];
+				a[j]=a[i]-a[j];
+				a[i]=a[i]-a[j];
+			}
+			
+		}
+	}
+	for(i=0;i<n;i++)
+	{
+		printf("%d",a[i]);
+	}
+}
+
+		
+
+
---
+++ C_Programs/pattern3.cpp
@@ -0,0 +1,21 @@
+#include<stdio.h>
+int main()
+{
+	int i,j,s,n;
+	printf("please enter the value of n:");
+	scanf("%d",&n);
+	for(i=0;i<n;i++)
+	{
+		for(s=0;s<n-i-1;s++)
+		{
+			printf(" ");		
+		}
+		for(j=0;j<i+1;j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+	
+
+}
