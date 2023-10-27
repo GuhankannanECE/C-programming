94dfa9abd0657fc29725ea7c5277f6351982a4cd
GuhankannanECE <143473399+GuhankannanECE@users.noreply.github.com>
2023-10-27 16:01:59 +0530

    Update addition and subtraction of two Matrices of same size.cpp

--- C_Programs/Lab 5/addition and subtraction of two Matrices of same size.cpp
+++ C_Programs/Lab 5/addition and subtraction of two Matrices of same size.cpp
@@ -2,24 +2,18 @@
 
 int main() {
     int row, col;
-
-    // Input the size of the matrices
     printf("Enter the number of rows: ");
     scanf("%d", &row);
     printf("Enter the number of columns: ");
     scanf("%d", &col);
 
     int matrix1[row][col], matrix2[row][col], sum[row][col], difference[row][col];
-
-    // Input elements for the first matrix
     printf("Enter the elements of the first matrix:\n");
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             scanf("%d", &matrix1[i][j]);
         }
     }
-
-    // Input elements for the second matrix
     printf("Enter the elements of the second matrix:\n");
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
@@ -27,21 +21,18 @@ int main() {
         }
     }
 
-    // Calculate the sum of the matrices
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             sum[i][j] = matrix1[i][j] + matrix2[i][j];
         }
     }
 
-    // Calculate the difference of the matrices
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
             difference[i][j] = matrix1[i][j] - matrix2[i][j];
         }
     }
 
-    // Print the sum of matrices
     printf("Sum of matrices:\n");
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
@@ -50,7 +41,6 @@ int main() {
         printf("\n");
     }
 
-    // Print the difference of matrices
     printf("Difference of matrices:\n");
     for (int i = 0; i < row; i++) {
         for (int j = 0; j < col; j++) {
