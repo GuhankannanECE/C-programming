acf62c20bcf18fd5fc3b25b33dc3d635a6b551b2
GuhankannanECE <143473399+GuhankannanECE@users.noreply.github.com>
2023-10-27 15:57:37 +0530

    Update 2D array of size 3x3 and print the matrix..cpp

--- C_Programs/Lab 5/2D array of size 3x3 and print the matrix..cpp
+++ C_Programs/Lab 5/2D array of size 3x3 and print the matrix..cpp
@@ -4,8 +4,6 @@ int main() {
     int matrix[3][3];
 
     printf("Enter the elements of the 3x3 matrix:\n");
-
-    // Input elements into the matrix
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             scanf("%d", &matrix[i][j]);
@@ -13,13 +11,11 @@ int main() {
     }
 
     printf("The 3x3 matrix is:\n");
-
-    // Print the matrix
     for (int i = 0; i < 3; i++) {
         for (int j = 0; j < 3; j++) {
             printf("%d ", matrix[i][j]);
         }
-        printf("\n"); // Move to the next row
+        printf("\n"); 
     }
 
     return 0;
