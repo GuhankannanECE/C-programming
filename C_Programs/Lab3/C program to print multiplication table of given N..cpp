f83a1c93442a4b5741aee1f107f744260bf75f13
GuhankannanECE <143473399+GuhankannanECE@users.noreply.github.com>
2023-10-27 15:37:41 +0530

    Update C program to print multiplication table of given N..cpp

--- C_Programs/Lab3/C program to print multiplication table of given N..cpp
+++ C_Programs/Lab3/C program to print multiplication table of given N..cpp
@@ -2,12 +2,8 @@
 
 int main() {
     int N;
-
-    // Input the number for which you want to print the multiplication table
     printf("Enter a number to print its multiplication table: ");
     scanf("%d", &N);
-
-    // Display the multiplication table for N
     printf("Multiplication table for %d:\n", N);
     for (int i = 1; i <= 10; i++) {
         printf("%d x %d = %d\n", N, i, N * i);
