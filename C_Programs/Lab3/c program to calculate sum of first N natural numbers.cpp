90c290fa1ca46c941a308b09384157ba4ce8b01d
GuhankannanECE <143473399+GuhankannanECE@users.noreply.github.com>
2023-10-27 15:44:41 +0530

    Update c program to calculate sum of first N natural numbers.cpp

--- C_Programs/Lab3/c program to calculate sum of first N natural numbers.cpp
+++ C_Programs/Lab3/c program to calculate sum of first N natural numbers.cpp
@@ -2,17 +2,12 @@
 
 int main() {
     int N, sum = 0;
-
     // Input the value of N
     printf("Enter the value of N: ");
     scanf("%d", &N);
-
-    // Calculate the sum of the first N natural numbers
     for (int i = 1; i <= N; i++) {
         sum += i;
     }
-
-    // Print the result
     printf("The sum of the first %d natural numbers is: %d\n", N, sum);
 
     return 0;
