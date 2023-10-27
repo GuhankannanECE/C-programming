ac8f14f12f75371da4865d868c7d5c5e6b99376e
GuhankannanECE <143473399+GuhankannanECE@users.noreply.github.com>
2023-10-27 15:20:16 +0530

    Update C program to find the sum of all digits of a given number.cpp

--- C_Programs/Lab3/C program to find the sum of all digits of a given number.cpp
+++ C_Programs/Lab3/C program to find the sum of all digits of a given number.cpp
@@ -2,18 +2,15 @@
 
 int main() {
     int num, digit, sum = 0;
-
-    // Input the number
     printf("Enter an integer: ");
     scanf("%d", &num);
 
     while (num != 0) {
-        digit = num % 10;  // Extract the last digit
-        sum += digit;      // Add the digit to the sum
-        num /= 10;         // Remove the last digit from the number
+        digit = num % 10; 
+        sum += digit;      
+        num /= 10;         
     }
 
     printf("The sum of the digits is: %d\n", sum);
-
     return 0;
 }
