ac68e58994e7510cd79f908c28ce2f7011722c7e
GuhankannanECE <143473399+GuhankannanECE@users.noreply.github.com>
2023-10-27 15:17:28 +0530

    Update C program to find sum of first and last digit.cpp

--- C_Programs/Lab3/C program to find sum of first and last digit.cpp
+++ C_Programs/Lab3/C program to find sum of first and last digit.cpp
@@ -2,23 +2,14 @@
 
 int main() {
     int num, firstDigit, lastDigit, sum;
-
-    // Input the number
     printf("Enter an integer: ");
     scanf("%d", &num);
-
-    // Find the last digit
     lastDigit = num % 10;
-
-    // Find the first digit by repeatedly dividing by 10 until there's only one digit left
     while (num >= 10) {
         num /= 10;
     }
     firstDigit = num;
-
-    // Calculate the sum of the first and last digits
     sum = firstDigit + lastDigit;
-
     printf("The sum of the first and last digits is: %d\n", sum);
 
     return 0;
